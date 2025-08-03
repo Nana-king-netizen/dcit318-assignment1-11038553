ff20b37b053ae99067f13e58bfe11df19a323158
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-03 13:29:52 +0000

    Add abstract Shape class with GetArea method
    Introduces a new abstract class `Shape` in the `DCIT318Assignment2` namespace. The class defines an abstract method `GetArea()` for derived classes to implement. Several `using` directives have also been added to support various functionalities.

---
+++ DCIT318Assignment2/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCIT318Assignment2
+{
+    internal abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+}
