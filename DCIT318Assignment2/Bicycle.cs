ba06b78294a36ecb36685e6633f81348a5b9f0a0
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-03 13:32:05 +0000

    Add Bicycle class implementing IMovable interface
    Introduced a new `Bicycle` class with a `Move()` method that prints "Bicycle is moving" to the console. Added necessary `using` directives for required namespaces.

---
+++ DCIT318Assignment2/Bicycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCIT318Assignment2
+{
+    internal class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+    
+    
+}
