2a2add18a2c394cf8f1141abba2e2f419aed332d
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-03 13:31:00 +0000

    Add Circle class with area calculation
    Introduces a new `Circle` class that inherits from `Shape`.
    Includes a `Radius` property, a constructor for initialization,
    and an overridden `GetArea` method to compute the area using
    the formula πr².

---
+++ DCIT318Assignment2/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCIT318Assignment2
+{
+    internal class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        
+    }
+}
