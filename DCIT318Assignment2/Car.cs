19f3140dd265817ef650769b6f98df3ffe49c848
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-03 13:32:39 +0000

    Add Car class implementing IMovable interface
    Introduces a new `Car` class with a `Move` method that prints "Car is moving" to the console. Also adds necessary `using` directives for required namespaces.

---
+++ DCIT318Assignment2/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCIT318Assignment2
+{
+    internal class Car : IMovable
+    {
+        public void Move()
+        { 
+        Console.WriteLine("Car is moving");
+        }
+    }
+}
