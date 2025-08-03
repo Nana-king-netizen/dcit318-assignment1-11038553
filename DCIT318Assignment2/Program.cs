ab6ebeccde9960bfc7fe42af2d833cb67b63204a
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-03 13:33:12 +0000

    Add Program class with animal and shape functionality
    Introduces a new `Program` class containing a `Main` method that creates instances of `Animal`, `Dog`, `Cat`, `Circle`, `Rectangle`, `Car`, and `Bicycle`. Implements sound and movement methods for animals and vehicles, and calculates and displays the area of a circle and a rectangle.

---
+++ DCIT318Assignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DCIT318Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Dog myDog = new Dog();
+            Cat myCat = new Cat();
+            
+            myAnimal.MakeSound(); 
+            myDog.MakeSound();
+            myCat.MakeSound();
+           
+            Console.WriteLine();
+
+            Circle circle = new Circle(5.0);
+            Console.WriteLine($"Circle Area: {circle.GetArea()}");
+
+            Rectangle rectangle = new Rectangle(4.0, 6.0);
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+
+            Console.WriteLine();
+
+            Car myCar = new Car();
+            Bicycle myBicycle = new Bicycle();
+
+            myCar.Move();
+            myBicycle.Move();
+
+
+        }
+    }
+}
