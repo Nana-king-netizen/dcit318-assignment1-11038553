3f7e387a7ac60ff5f4640eea9597b861795be3b8
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-03 13:30:31 +0000

    Add Rectangle class with area calculation
    Introduces a new `Rectangle` class inheriting from `Shape`.
    Includes properties for `Length` and `Width`, a constructor
    for initialization, and an overridden `GetArea` method
    to compute the rectangle's area.

---
+++ DCIT318Assignment2/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCIT318Assignment2
+{
+    internal class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        public override double GetArea()
+        {
+            return Length * Width;
+        }
+    }
+    }
+
+
