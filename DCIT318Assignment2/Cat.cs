ddc5effea6e2490608656bed308d397649a09948
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-03 13:24:56 +0000

    Add Cat class with MakeSound method
    Introduces a new `Cat` class that inherits from `Animal` in the `DCIT318Assignment2` namespace. The `MakeSound` method outputs "Meow" to the console. Also includes necessary `using` directives for various namespaces.

---
+++ DCIT318Assignment2/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCIT318Assignment2
+{
+    internal class Cat:Animal
+    {
+        public void MakeSound() 
+        { 
+            Console.WriteLine("Meow"); 
+        }
+    }
+    
+}
